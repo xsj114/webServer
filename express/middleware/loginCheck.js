b6097b4bca0b430d3b50358496ec15adb2fc4b43
xushijie <xushijie@zuoshouyisheng.com>
2019-07-03 10:17:47 +0800

    submit

--- express/middleware/loginCheck.js
+++
@@ -1,9 +0,0 @@
-const {ErrModel} = require('../model/resModel')
-
-module.exports = (req,res,next) => {
-    if (req.session.username) {
-        next()
-        return
-    }
-    res.json(new ErrModel('未登录'))
-}
